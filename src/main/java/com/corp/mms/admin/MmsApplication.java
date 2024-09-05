29c49876c32d3a7cc0692d113225b49793626103
bandalgomsu <q63530@naver.com>
2024-09-05 11:28:19 +0900

    init

---
+++ src/main/java/com/corp/mms/admin/MmsApplication.java
@@ -0,0 +1,13 @@
+package com.corp.mms.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmsApplication.class, args);
+    }
+
+}
